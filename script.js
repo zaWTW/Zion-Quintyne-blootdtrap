17465a62008ee55e99a48a13676a84aff1d20195
zaWTW <zionquintyne1@gmail.com>
2024-08-06 16:04:25 +0000

    I made an age verifacator today

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console. log("Zion Quintyne")
+//console. log("Zion Quintyne")
+
+//var prompt = prompt('What is your name?')
+
+//console.log(prompt)
+
+function verifyage(age) {
+  var age = prompt('What is your age?');
+
+  if (age < 18 && typeof age === "string") {
+    alert('You are a minor!');
+  } else if (age >= 18 && age <= 60) {
+    alert('You a grown a$$ man!')
+  } else {
+    alert('Dam you are old!')
+  }
+}
